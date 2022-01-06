b3c231f235271ade4fb4e56b9f65941e84950f54
DELPITEC by Rafael Del Pino <58537514+delpitec@users.noreply.github.com>
2022-01-06 00:38:02 -0300

    Update timetogo.cpp

--- timetogo.cpp
+++ timetogo.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 class ConfigurationTime{
     public:
+
+        unsigned int jobBeginHour;
+        unsigned int jobBeginMinute;
+        unsigned int lunchDurationHour;
+        unsigned int lunchDurationMinute;
+        unsigned int jobDurationHour;
+        unsigned int jobDurationMinute;
+
         ConfigurationTime(){
             jobBeginHour = 0;
             jobBeginMinute = 0;
@@ -16,14 +24,30 @@ class ConfigurationTime{
             jobDurationHour = 0;
             jobDurationMinute = 0;
 
-            lastDayConfig = 0;
-            lastMonthConfig = 0;
-            lastYearConfig = 0; 
+            SetLastConfigDay(0);
+            SetLastConfigMonth(0);
+            SetLastConfigYear(0);
         }
 
         ConfigurationTime(std::string filePath){
             _filePath = filePath;
             ReceiveConfigurationFromFile();
+        }
+
+        bool FileConfigurationTimeIsUpdated(void){
+            // Get current time
+            std::tm tmFileTime {0};
+            std::time_t timeNow = std::time(0);
+            std::tm* tmTimeNow = localtime(&timeNow);
+            
+            if (tmTimeNow->tm_mday != GetLastConfigDay() ||
+                tmTimeNow->tm_mon + 1  != GetLastConfigMonth() || 
+                tmTimeNow->tm_year - 100 != GetLastConfigYear() ){
+                    return false;
+            }
+            else{
+                return true;
+            }
         }
 
         void SetJobBeginFromKeyboard(void){
@@ -95,46 +119,87 @@ class ConfigurationTime{
             jobDurationHour = stoi(line.at(4));
             jobDurationMinute = stoi(line.at(5));
 
-            lastDayConfig = stoi(line.at(6));
-            lastMonthConfig = stoi(line.at(7));
-            lastYearConfig = stoi(line.at(8));
-
+            SetLastConfigDay(stoi(line.at(6)));
+            SetLastConfigMonth(stoi(line.at(7)));
+            SetLastConfigYear(stoi(line.at(8)));
             configFile.close();
         }
-        void SendConfigurationToFile(tm* newCongigData){
+
+        void WriteFileAll(void){
             std::ofstream configFile;
             configFile.open(_filePath, std::ofstream::out | std::ofstream::trunc);
-            configFile << "BEGIN_HOUR          = " << jobBeginHour << endl
-                       << "BEGIN_MIN           = " << jobBeginMinute << endl
+            configFile << "JOB_BEGIN_HOUR      = " << jobBeginHour << endl
+                       << "JOB_BEGIN_MIN       = " << jobBeginMinute << endl
                        << "LUNCH_DURATION_HOUR = " << lunchDurationHour << endl
                        << "LUNCH_DURATION_MIN  = " << lunchDurationMinute << endl
                        << "JOB_DURATION_HOUR   = " << jobDurationHour << endl
-                       << "JOB_DURATION_MIN    = " << jobDurationMinute << endl
-                       << "LAST_DAY_CONFIG     = " << newCongigData->tm_mday << endl
-                       << "LAST_MON_CONFIG     = " << newCongigData->tm_mon + 1 << endl
-                       << "LAST_YEAR_CONFIG    = " << newCongigData->tm_year - 100 << endl;
+                       << "JOB_DURATION_MIN    = " << jobDurationMinute << endl;
+            WriteFileUpdateLastConfigData();
             configFile.close();
         }
-        
-        unsigned int jobBeginHour;
-        unsigned int jobBeginMinute;
-        unsigned int lunchDurationHour;
-        unsigned int lunchDurationMinute;
-        unsigned int jobDurationHour;
-        unsigned int jobDurationMinute;
-        unsigned int lastDayConfig;
-        unsigned int lastMonthConfig;
-        unsigned int lastYearConfig;
 
-    private:    
+        void WriteFileUpdateLastConfigData(void){
+            std::vector<std::string> line;
+            std::string input;
+            fstream inputConfigFile;
+            fstream outputConfigFile;
+            
+            
+            // read all Configuration file
+            inputConfigFile.open(_filePath, std::fstream::in);
+            while (std::getline(inputConfigFile, input)){
+                line.push_back(input);   
+            }
+            inputConfigFile.close();
+
+
+            // write Configuration file [refresh last day, month and year file config for NOW date]
+            outputConfigFile.open(_filePath, std::fstream::out | std::fstream::trunc);
+            for (unsigned int i = 0; i < 6 ; i++){
+                outputConfigFile << line.at(i) << endl;
+            } 
+            // Get current time
+            std::tm tmFileTime {0};
+            std::time_t timeNow = std::time(0);
+            std::tm* tmTimeNow = localtime(&timeNow); 
+            
+            outputConfigFile  << "LAST_CONFIG_DAY     = " << tmTimeNow->tm_mday << endl
+                              << "LAST_CONFIG_MON     = " << tmTimeNow->tm_mon + 1 << endl
+                              << "LAST_CONFIG_YEAR    = " << tmTimeNow->tm_year - 100 << endl;
+            outputConfigFile.close();
+        }
+
+        
+        void SetLastConfigDay(unsigned int value){_lastConfig.day = value; }
+        void SetLastConfigMonth(unsigned int value){_lastConfig.month = value; }
+        void SetLastConfigYear(unsigned int value){_lastConfig.year = value; }
+        void SetJobRemainingTimeHour(unsigned int value){_jobRemainingTime.hour = value; }
+        void SetJobRemainingTimeMinute(unsigned int value){_jobRemainingTime.minute = value; }
+        unsigned int GetLastConfigDay(void){ return _lastConfig.day; }
+        unsigned int GetLastConfigMonth(void){ return _lastConfig.month; }
+        unsigned int GetLastConfigYear(void){ return _lastConfig.year; }
+        unsigned int GetJobRemainingTimeHour(void){ return _jobRemainingTime.hour; }
+        unsigned int GetJobRemainingTimeMinute(void){ return _jobRemainingTime.minute; }
+
+    private:
         string _filePath;
+        enum TimeAndDateUnit {Hour, Minute, Day, Month, Year};
+        struct LAST_CONFIG {unsigned int day ; unsigned int month; unsigned int year;} _lastConfig;
+        struct JOB_REMAINING_TIME{unsigned int hour; unsigned int minute;} _jobRemainingTime;
+
+        unsigned int CalculateJobRemainingTime(TimeAndDateUnit unit, struct JOB_REMAINING_TIME time){
+            return 0;
+        }
+
+
+
 };
 
 std::tm RemainingTime(std::tm* end, std::tm* begin);
 
 int main(int argc, char* argv[])
 {
-    ConfigurationTime config = ConfigurationTime("/home/rafael.pino/time-to-go-app/Configuration");
+    ConfigurationTime config = ConfigurationTime("Configuration");
 
     // Get current time
     std::tm tmFileTime {0};
@@ -142,20 +207,22 @@ int main(int argc, char* argv[])
     std::tm* tmTimeNow = localtime(&timeNow);
 
     // check if Configuration read from file is updated
-    if (tmTimeNow->tm_mday != config.lastDayConfig ||
-        tmTimeNow->tm_mon + 1  != config.lastMonthConfig || 
-        tmTimeNow->tm_year - 100 != config.lastYearConfig){
-
+    if (!config.FileConfigurationTimeIsUpdated()){
         config.SetJobBeginFromKeyboard();
-        config.SendConfigurationToFile(tmTimeNow);
+        config.WriteFileAll();
     }
 
     std::tm tmEnd = {0};
-    tmEnd.tm_hour = config.jobBeginHour + config.lunchDurationHour + config.jobDurationHour;
-    tmEnd.tm_min = config.jobBeginMinute + config.lunchDurationMinute + config.jobDurationMinute;
+    tmEnd.tm_hour = 2;//config.jobBeginHour + config.lunchDurationHour + config.jobDurationHour;
+    tmEnd.tm_min =  70;// config.jobBeginMinute + config.lunchDurationMinute + config.jobDurationMinute;
+
+    cout << tmEnd.tm_hour << "h" << tmEnd.tm_min << "min"<< endl;
+    
 
     std::tm timeRemaining = RemainingTime(&tmEnd, tmTimeNow);
-        
+
+    return (0);
+
     char buffer[80];
     printf("\n####################################\n");
     printf("#                                  #\n"  );
@@ -184,9 +251,12 @@ std::tm RemainingTime(std::tm* end, std::tm* begin){
     
     time_t difftimeSeconds = difftime(mktime(end), mktime(begin));
 
+    mktime(end);
+
+    cout << end->tm_hour << "h" << end->tm_min << "min"<< endl;
+
     ret.tm_hour = difftimeSeconds / 3600;
     ret.tm_min = (difftimeSeconds / 60) - (ret.tm_hour * 60);
 
     return ret;
 }
-
